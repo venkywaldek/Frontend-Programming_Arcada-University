704c07467a4f0a8046a7ad87b1053750654bbdc4
Venkat  Battina <venkatneoiit@gmail.com>
2025-11-11 10:00:56 +0200

    lektion-5 changes

---
+++ lektion-5/js/greetings.js
@@ -0,0 +1,13 @@
+console.log('greetings');
+const defaultGreeting = 'hello';
+
+function createGreeting(greeting, name) {
+  return `${greeting} ${name}`;
+}
+export function hello(name) {
+  return `Hello ${name}!`;
+}
+
+export function morjens(name) {
+  return `Morjens ${name}!`;
+}
---
+++ lektion-5/js/main.js
@@ -0,0 +1,4 @@
+import { hello, morjens } from './greetings.js';
+
+console.log(hello('ahmak'));
+console.log(morjens('ahmak'));
