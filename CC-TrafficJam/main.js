505b384b5343dcf357301c08cf7a33e6b584ca06
Venkat  Battina <venkatneoiit@gmail.com>
2025-11-18 10:01:33 +0200

    CC - traffic jam

---
+++ CC-TrafficJam/main.js
@@ -0,0 +1,24 @@
+const COLORS = {
+  red: '#ff0000',
+  yellow: '#ffaa00',
+  green: 'lime',
+};
+
+function resetLights() {
+  for (const light of document.querySelectorAll(`#lights > div`)) {
+    light.style.backgroundColor = 'black';
+  }
+}
+function activateLight(lightNumber) {
+  resetLights();
+  const light = document.querySelector(
+    `#lights > div:nth-child(${lightNumber})`
+  );
+  const color = light.dataset.color;
+  light.style.backgroundColor = COLORS[color]; //Ta färgen från const COLORS
+}
+
+setInterval(() => {
+  const randNumber = Math.ceil(Math.random() * 3);
+  activateLight(randNumber);
+}, 500);
